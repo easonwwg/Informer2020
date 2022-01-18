ae51cbaab5c5393e203198568eba52f46d250834
easonwwg <990793233@qq.com>
2022-01-18 15:19:10 +0800

    提交最新的修改代码

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,33 +1,47 @@
 import numpy as np
 
+
 def RSE(pred, true):
-    return np.sqrt(np.sum((true-pred)**2)) / np.sqrt(np.sum((true-true.mean())**2))
+    return np.sqrt(np.sum((true - pred) ** 2)) / np.sqrt(np.sum((true - true.mean()) ** 2))
+
 
 def CORR(pred, true):
-    u = ((true-true.mean(0))*(pred-pred.mean(0))).sum(0) 
-    d = np.sqrt(((true-true.mean(0))**2*(pred-pred.mean(0))**2).sum(0))
-    return (u/d).mean(-1)
+    u = ((true - true.mean(0)) * (pred - pred.mean(0))).sum(0)
+    d = np.sqrt(((true - true.mean(0)) ** 2 * (pred - pred.mean(0)) ** 2).sum(0))
+    return (u / d).mean(-1)
+
 
 def MAE(pred, true):
-    return np.mean(np.abs(pred-true))
+    return np.mean(np.abs(pred - true))
+
 
 def MSE(pred, true):
-    return np.mean((pred-true)**2)
+    return np.mean((pred - true) ** 2)
+
 
 def RMSE(pred, true):
     return np.sqrt(MSE(pred, true))
 
+
 def MAPE(pred, true):
     return np.mean(np.abs((pred - true) / true))
+
 
 def MSPE(pred, true):
     return np.mean(np.square((pred - true) / true))
 
+
+# add
+def RSquare(y_pred, y_true):
+    return 1 - np.sum((y_true - y_pred) ** 2) / np.sum((y_true - np.mean(y_true)) ** 2)
+
+
+# modify
 def metric(pred, true):
     mae = MAE(pred, true)
     mse = MSE(pred, true)
     rmse = RMSE(pred, true)
     mape = MAPE(pred, true)
     mspe = MSPE(pred, true)
-    
-    return mae,mse,rmse,mape,mspe
+    rsquare = RSquare(pred, true)
+    return mae, mse, rmse, mape, mspe, rsquare
